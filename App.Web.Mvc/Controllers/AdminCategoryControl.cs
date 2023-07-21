fde6feccf346c0b55238c0b81e8fa5a6ef299acd
enbiya onar <kartal.enbiya@hotmail.com>
2023-07-21 22:30:21 +0300

    Controllers dosyası içine yeni şeyler eklendi.

---
+++ App.Web.Mvc/Controllers/AdminCategoryControl.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Controllers
+{
+    public class AdminCategoryControl : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ App.Web.Mvc/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace App.Web.Mvc.Models
+{
+    public class Admin : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
